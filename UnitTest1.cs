29192acbe125753ab96fe5c29aa2aa25aa8ff90f
Alexey Gaidykov <swiftgaida@list.ru>
2024-01-12 16:56:47 +0300

    Optimization

--- UnitTest1.cs
+++ UnitTest1.cs
@@ -9,20 +9,24 @@ public class Tests : PageTest
     [Test]
     public async Task MyTest()
     {   
+        //SMS code
         const string SMSCode = "999666";
+        //Page object
+        async Task ClickContinueButton()
+        {
+        await Page.GetByRole(AriaRole.Button, new() { Name = "Continue" }).ClickAsync();
+        }
         
         //URL
         await Page.GotoAsync("https://test-jpwallet.pay2pay.tech/#/auth/steps");
         //Create 
         await Page.GetByRole(AriaRole.Textbox).ClickAsync();
         await Page.GetByRole(AriaRole.Textbox).FillAsync("+79298905138");
-        await Page.GetByRole(AriaRole.Button, new() { Name = "Continue" }).ClickAsync();
+        await ClickContinueButton();
         await Page.GetByRole(AriaRole.Button, new() { Name = "Create" }).ClickAsync();
         
-        //sms
-        await Task.Delay(3000);
+        //SMS
         await Page.FillAsync("input[name='smsConfirmationCode']", SMSCode);
-        //await Page.Keyboard.TypeAsync(SMSCode);
         
         //Info
         await Page.GetByPlaceholder("Firstname").ClickAsync();
@@ -32,7 +36,7 @@ public class Tests : PageTest
         await Page.GetByPlaceholder("Date of birth").ClickAsync();
         await Page.GetByPlaceholder("Date of birth").FillAsync("20.11.2000");
         await Page.GetByText("Male", new() { Exact = true }).ClickAsync();
-        await Page.GetByRole(AriaRole.Button, new() { Name = "Continue" }).ClickAsync();
+        await ClickContinueButton();
 
         //Adress
         await Page.GetByPlaceholder("Address").ClickAsync();
@@ -41,7 +45,7 @@ public class Tests : PageTest
         await Page.GetByPlaceholder("City").FillAsync("asd");
         await Page.GetByPlaceholder("Postal code").ClickAsync();
         await Page.GetByPlaceholder("Postal code").FillAsync("asd");
-        await Page.GetByRole(AriaRole.Button, new() { Name = "Continue" }).ClickAsync();
+        await ClickContinueButton();
         
         //Document
         await Page.GetByPlaceholder("Document type").ClickAsync();
@@ -52,42 +56,37 @@ public class Tests : PageTest
         await Page.GetByPlaceholder("Release date").FillAsync("09.01.2024");
         await Page.GetByPlaceholder("Valid to").ClickAsync();
         await Page.GetByPlaceholder("Valid to").FillAsync("09.01.2024");
-        await Page.GetByRole(AriaRole.Button, new() { Name = "Continue" }).ClickAsync();
-        // await Task.Delay(2000);
-        //Checking that there was no transition to the next step
-        var element = await Page.WaitForSelectorAsync(".h2.mb-4");
-        // if (element != null) {
-        //     Console.WriteLine("Остались на шаге");
-        // } else {
-        //     Console.WriteLine("Перешли");
-        // }
-        Assert.IsNotNull(element,"Перешел на след шаг");
+        await ClickContinueButton();
+        await Task.Delay(2000);
 
-        
-        
-        //input
+        //Checking that there was no transition to the next step - *не могу с этим разобраться*
+        // var element =  await Page.WaitForSelectorAsync(".mb-4");
+        // Assert.IsTrue(element != null,"Fill in your document details не найден");
+        // //Take screen
+        // await Page.ScreenshotAsync(new()
+        // {
+        //      Path = "/Users/alexeygaidykov/PlaywrightTests/screenshot1.png",
+        // });
+
+
+        //ValidationError
         var validationError = await Page.WaitForSelectorAsync(".app__field__input--error");
-        Assert.IsNotNull(validationError, "Элемент с ошибкой не отображен на странице.");
-
-        // if (validationError != null) {
-        //         Console.WriteLine("Элемент отображен на странице.");
-        // //Checking for what error text appeared on the screen
-        var spanElement = await Page.QuerySelectorAsync("span:has-text('Date cannot be later than ')");
-        Assert.IsNotNull(spanElement, "Элемент с текстом ошибки не найден на странице.");
-
-        // if (spanElement != null) {
-        //             Console.WriteLine("Текст ошибки совпадает.");
-        //         } else {
-        //             Console.WriteLine("Элемент НЕ найден на странице.");
-        //         }
-        // Assert.IsNotNull(spanElement, "Элемент с текстом ошибки не найден на странице.");
+        Assert.IsTrue(validationError != null, "Элемент с ошибкой не отображен на странице.");
+        //Take screen
         await Page.ScreenshotAsync(new()
         {
-             Path = "/Users/alexeygaidykov/PlaywrightTests/screenshot.png",
+             Path = "/Users/alexeygaidykov/PlaywrightTests/screenshot2.png",
         });
 
-        // }
-        
+        //Checking for what error text appeared on the screen
+        var spanElement = await Page.QuerySelectorAsync("span:has-text('Date cannot be later than ')");
+        Assert.IsTrue(spanElement != null, "Элемент с текстом ошибки не найден на странице.");
+
+        //Take screen
+        await Page.ScreenshotAsync(new()
+        {
+             Path = "/Users/alexeygaidykov/PlaywrightTests/screenshot3.png",
+        });
 
     }
 }
--- obj/Debug/net8.0/PlaywrightTests.AssemblyInfo.cs
+++ obj/Debug/net8.0/PlaywrightTests.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PlaywrightTests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+68f200f0a630616c8c46f79e0b06d64886affd62")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("PlaywrightTests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PlaywrightTests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
